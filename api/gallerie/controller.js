28722e70912c9a3d2b9ae0cfd82012514c676e99
Bafarouk <44127908+Bafarouk@users.noreply.github.com>
2021-03-06 23:45:02 +0100

    adding controller methodes

--- api/gallerie/controller.js
+++ api/gallerie/controller.js
@@ -15,3 +15,48 @@ async function addGallerie(req,res){
     }
     return res.status(400).end();
 }
+
+
+async function deleteGallerie(req,res){
+    let gallerie_id = req.query.id;
+    const gallerie = await galleries.deleteOneById(gallerie_id);
+    if(gallerie){
+        return res.status(200).send();
+    }
+    return res.status(404).send();
+}
+
+async function getAllGalleries(req,res){
+    const gallerie = await galleries.find();
+    if(gallerie){
+        return res.status(200).send(gallerie);
+    }
+    return res.status(404).send();
+}
+
+async function getGallerieById(req,res){
+    const gallerie = await galleries.findOneById(req.query.gallerieId);
+    if(gallerie){
+        return res.status(200).send(gallerie);
+    }
+    return res.status(404).send();
+}
+
+async function updateGallerie(req,res){
+    let gallerie_id = req.query.gallerieId;
+    const gallerie = await galleries.updateOne(gallerie_id, req.body);
+    if(gallerie){
+        return res.status(200).send(gallerie);
+    }
+    return res.status(404).send();
+}
+
+
+
+module.exports = {
+    addGallerie,
+    deleteGallerie,
+    getAllGalleries,
+    getGallerieById,
+    updateGallerie
+}; 

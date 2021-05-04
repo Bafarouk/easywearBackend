4550ddc46e5809ec88b0175a8196a1e7ab573188
AlouiOmar97 <60584294+AlouiOmar97@users.noreply.github.com>
2021-05-04 22:12:18 +0100

    Update order.js

--- models/order.js
+++ models/order.js
@@ -117,3 +117,241 @@ module.exports = {
   findUsersByItem,
   findUsersByItemOrder,
 };
+const mongoose = require("mongoose");
+const Joi = require("../lib/joi");
+const dateReaction = require("../lib/date");
+
+const order = mongoose.Schema({
+  reference: String,
+  total: Number,
+  date_creation: Date,
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  product_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+  },
+});
+const joiOrderSchema = Joi.object({
+  _id: Joi.objectId(),
+  reference: Joi.string().required(),
+  total: Joi.number().positive().required(),
+  date_creation: Joi.date().default(
+    () => dateReaction.getDate(),
+    "date of creation"
+  ),
+  user_id: Joi.objectId().required(),
+  product_id: Joi.objectId().required(),
+});
+
+function collection() {
+  return mongoose.model("orders", order);
+}
+
+function _validateSchema(order) {
+  return Joi.attempt(order, joiOrderSchema);
+}
+
+async function findOrderbyId(id) {
+  const order = await collection().findOne({
+    _id: id,
+  });
+  return order;
+}
+
+async function findItemsByUser(userId, orderType) {
+  const orders = await collection().find(
+    {
+      user_id: userId,
+      reference: orderType,
+    },
+    { _id: 0, product_id: 1 }
+  );
+  return orders;
+}
+
+async function findItemsByUserId(userId) {
+  //const orders = await collection().find({ user_id: userId });
+  const orders = await collection().aggregate([
+    {
+      $lookup: {
+        from: "products",
+        localField: "product_id",
+        foreignField: "_id",
+        as: "products",
+      },
+    },
+  ]);
+  return orders;
+}
+
+async function findUsersByItem(productId) {
+  const orders = await collection().find({
+    product_id: productId,
+  });
+  return orders;
+}
+
+async function findUsersByItemOrder(productId, type) {
+  const orders = await collection().find(
+    {
+      product_id: productId,
+      reference: type,
+    },
+    { _id: 0, user_id: 1 }
+  );
+  return orders;
+}
+
+async function insertOne(order) {
+  const order_validate = _validateSchema(order);
+  if (order_validate) {
+    const order_returned = await collection().insertMany(order_validate);
+    return order_returned;
+  }
+  return null;
+}
+
+async function deleteOrder(order_id) {
+  const order = await collection().findOne({
+    _id: order_id,
+  });
+  /*   console.log("Order to delete");
+  console.log(order); */
+  if (order) {
+    await collection().deleteOne({ _id: order._id });
+    return true;
+  }
+  return false;
+}
+
+module.exports = {
+  insertOne,
+  deleteOrder,
+  findOrderbyId,
+  findItemsByUser,
+  findItemsByUserId,
+  findUsersByItem,
+  findUsersByItemOrder,
+};
+const mongoose = require("mongoose");
+const Joi = require("../lib/joi");
+const dateReaction = require("../lib/date");
+
+const order = mongoose.Schema({
+  reference: String,
+  total: Number,
+  date_creation: Date,
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  product_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+  },
+});
+const joiOrderSchema = Joi.object({
+  _id: Joi.objectId(),
+  reference: Joi.string().required(),
+  total: Joi.number().positive().required(),
+  date_creation: Joi.date().default(
+    () => dateReaction.getDate(),
+    "date of creation"
+  ),
+  user_id: Joi.objectId().required(),
+  product_id: Joi.objectId().required(),
+});
+
+function collection() {
+  return mongoose.model("orders", order);
+}
+
+function _validateSchema(order) {
+  return Joi.attempt(order, joiOrderSchema);
+}
+
+async function findOrderbyId(id) {
+  const order = await collection().findOne({
+    _id: id,
+  });
+  return order;
+}
+
+async function findItemsByUser(userId, orderType) {
+  const orders = await collection().find(
+    {
+      user_id: userId,
+      reference: orderType,
+    },
+    { _id: 0, product_id: 1 }
+  );
+  return orders;
+}
+
+async function findItemsByUserId(userId) {
+  //const orders = await collection().find({ user_id: userId });
+  const orders = await collection().aggregate([
+    {
+      $lookup: {
+        from: "products",
+        localField: "product_id",
+        foreignField: "_id",
+        as: "products",
+      },
+    },
+  ]);
+  return orders;
+}
+
+async function findUsersByItem(productId) {
+  const orders = await collection().find({
+    product_id: productId,
+  });
+  return orders;
+}
+
+async function findUsersByItemOrder(productId, type) {
+  const orders = await collection().find(
+    {
+      product_id: productId,
+      reference: type,
+    },
+    { _id: 0, user_id: 1 }
+  );
+  return orders;
+}
+
+async function insertOne(order) {
+  const order_validate = _validateSchema(order);
+  if (order_validate) {
+    const order_returned = await collection().insertMany(order_validate);
+    return order_returned;
+  }
+  return null;
+}
+
+async function deleteOrder(order_id) {
+  const order = await collection().findOne({
+    _id: order_id,
+  });
+  /*   console.log("Order to delete");
+  console.log(order); */
+  if (order) {
+    await collection().deleteOne({ _id: order._id });
+    return true;
+  }
+  return false;
+}
+
+module.exports = {
+  insertOne,
+  deleteOrder,
+  findOrderbyId,
+  findItemsByUser,
+  findItemsByUserId,
+  findUsersByItem,
+  findUsersByItemOrder,
+};
